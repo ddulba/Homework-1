2b786b7244bd42b0ab6b85e325b2caa150a9fa9d
ddulba <darek.dulba@gmail.com>
2020-05-17 09:15:02 +0200

    Ex1 done

---
+++ src/pl/javapoz25/Main.java
@@ -0,0 +1,28 @@
+package pl.javapoz25;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static boolean isPrime(int var2) {
+                
+    }
+
+    public static void main(String[] args) {
+
+        System.out.println("Exercise 1: ");
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Podaj liczbę, którą chcesz pomnożyć: ");
+        int var = scan.nextInt();
+
+        for (int i = 0; i <= 10; i++) {
+            System.out.println(var + " * " + i + " = " + (var * i));
+        }
+        System.out.println();
+
+        System.out.println("Exercise 2: ");
+
+
+    }
+
+}
