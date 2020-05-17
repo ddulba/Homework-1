f5bf6debdd43168e68868a5070147ad503a22cab
ddulba <darek.dulba@gmail.com>
2020-05-17 10:10:02 +0200

    Ex4 done

--- src/pl/javapoz25/Main.java
+++ src/pl/javapoz25/Main.java
@@ -21,6 +21,21 @@ public class Main {
         return isPrimeNumber;
     }
 
+//    public static int sumOfXY (int x, int y) {
+//
+//    }
+
+    public static void tableEx4 (int[] tab) {
+        int tmp = tab[0];
+        tab[0] = tab[2];
+        tab[2] = tmp;
+
+        for (int i = 0; i < tab.length; i++) {
+            System.out.println(tab[i]);
+        }
+
+    }
+
     public static void main(String[] args) {
 
         System.out.println("Exercise 1: ");
@@ -37,6 +52,14 @@ public class Main {
         System.out.print("Podaj liczbę: ");
         boolean result = isPrime(scan.nextInt());
         System.out.println(result);
+        System.out.println();
+
+        System.out.println("Exercise 3: ");
+
+        System.out.println("Exercise 4: ");
+        int[] tab = new int[] {1, 2, 3};
+        tableEx4(tab);
+
     }
 
 }
